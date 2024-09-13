6ad3db6cbbf0f80b7ef701db497f20d19672337e
Abhi - ROG <ag01portfolio@gmail.com>
2024-09-13 21:48:28 +0530

    fix

--- src/redux/reducers/productsReducer.js
+++ src/redux/reducers/productsReducer.js
@@ -11,3 +11,15 @@ export const productsReducer = (state = intialState, { type, payload }) => {
       return state;
   }
 };
+
+export const selectedProductsReducer = (state = {}, { type, payload }) => {
+  console.log(type);
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCT:
+      return { ...state, ...payload };
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
+    default:
+      return state;
+  }
+};
