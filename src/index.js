8f1d26b7dce51153a805e50481aa597453734419
Abhi - ROG <ag01portfolio@gmail.com>
2024-09-16 20:18:39 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-
+// console.log();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
